6de14340b28b4381df754f6079d395069e0eed5e
barondandi <barondandi@gmail.com>
2021-06-25 18:30:13 +0200

    Adding labs notes and code

--- labs/python/aggr.py
+++ labs/python/aggr.py
@@ -1,1 +1,11 @@
-#
+#! /usr/bin/env python3.7
+from netapp_ontap import config, HostConnection
+from netapp_ontap.resources import Aggregate
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+conn = HostConnection("192.168.0.101", username = "admin", password = "Netapp1!", verify = False)
+config.CONNECTION = conn
+aggr = Aggregate()
+aggr.get()
+print(aggr)
