6b30b9b6ac214ca233dace07d374c0744a315ba7
victorfrancoramos <formeitor@gmail.com>
2022-05-05 15:47:41 +0200

    Added code for LABs - M2

--- REST API Solutions/M2_Creation of a Quota Tree/qtree.py
+++ REST API Solutions/M2_Creation of a Quota Tree/qtree.py
@@ -15,6 +15,46 @@ import logging
 from netapp_ontap import config, HostConnection, NetAppRestError
 from netapp_ontap.resources import Qtree, QuotaRule, QosPolicy
 
+def parse_args() -> argparse.Namespace:
+    """Parse the command line arguments from the user"""
+
+    parser = argparse.ArgumentParser(
+        description="This script will create a new qtree."
+    )
+    parser.add_argument(
+        "-c", "--cluster", required=True, help="API server IP:port details"
+    )
+    parser.add_argument(
+        "-v", "--volume_name", required=True, help="Volume name to create qtree from"
+    )
+    parser.add_argument(
+        "-vs", "--vserver_name", required=True, help="SVM to create the volume from"
+    )
+    parser.add_argument(
+        "-q", "--qtree_name", required=True, help="QTree to create the qutoa tree"
+    )
+    parser.add_argument(
+        "-qos", "--qos_policy_name", required=True, help="QoS Policy to create on the SVM"
+    )
+    parser.add_argument(
+        "-sh", "--space_hard", required=True, help="Hard limit on space in bytese"
+    )
+    parser.add_argument(
+        "-fh", "--file_hard", required=True, help="hard limit on files in bytes"
+    )
+    parser.add_argument(
+        "-un", "--user_name", required=True, help="User name who can access the qtree"
+    )
+    parser.add_argument("-u", "--api_user", default="admin", help="API Username")
+    parser.add_argument("-p", "--api_pass", help="API Password")
+    parsed_args = parser.parse_args()
+
+    # collect the password without echo if not already provided
+    if not parsed_args.api_pass:
+        parsed_args.api_pass = getpass()
+
+    return parsed_args
+
 def create_qtree(volume_name: str, vserver_name: str, qtree_name: str) -> None:
     data = {
         'name': qtree_name,
@@ -35,13 +75,40 @@ def create_qtree(volume_name: str, vserver_name: str, qtree_name: str) -> None:
 
 def create_policy_rule(volume_name: str, vserver_name: str, qtree_name: str, user_name: str,
 space_hard: int, file_hard: int) -> None:
+'''
+    resource.svm = {"name": "svm1"}
+    resource.volume = {"name": "vol1"}
+    resource.type = "user"
+    resource.users = [{"name": "jsmith"}]
+    resource.qtree = {"name": "qt1"}
+    resource.user_mapping = "on"
+    resource.space = {"hard_limit": 8192, "soft_limit": 1024}
+    resource.files = {"hard_limit": 20, "soft_limit": 10}
+
+Required properties:
+    svm.uuid or svm.name - Existing SVM in which to create the qtree.
+    volume.uuid or volume.name - Existing volume in which to create the qtree.
+    type - Quota type for the rule. This type can be user, group, or tree.
+    users.name or user.id - If the quota type is user, this property takes the user name or user ID. For default user quota rules, the user name must be specified as "".
+    group.name or group.id - If the quota type is group, this property takes the group name or group ID.
+     For default group quota rules, the group name must be specified as "".
+    qtree.name - Qtree for which to create the rule. For default tree rules, the qtree name must be specified as "".
+Recommended optional properties:
+    space.hard_limit - Specifies the space hard limit, in bytes. If less than 1024 bytes, the value is rounded up to 1024 bytes.
+    space.soft_limit - Specifies the space soft limit, in bytes. If less than 1024 bytes, the value is rounded up to 1024 bytes.
+    files.hard_limit - Specifies the hard limit for files.
+    files.soft_limit - Specifies the soft limit for files.
+    user_mapping - Specifies the user_mapping. This property is valid only for quota policy rules of type user.
+'''
     data = {
-        'qtree': {'name': qtree_name},
-        'volume': {'name': volume_name},
         'svm': {'name': vserver_name},
-        'files': {'hard_limit': file_hard, 'soft_limit': 100},
+        'volume': {'name': volume_name},
+        'type': 'user',
+        'users': {'name': user_name},
+        'qtree': {'name': qtree_name},
+        'user_mapping': 'on',
         'space': {'hard_limit': space_hard, 'soft_limit': 100},
-        'type': 'user'
+        'files': {'hard_limit': file_hard, 'soft_limit': 100},
     }
     quotarule = QuotaRule(**data)
     try:
